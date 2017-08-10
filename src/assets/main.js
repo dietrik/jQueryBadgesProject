c9fe1ded507f973804fe5d07bc50af668aa28a4c
Brian Sullivan <dietrik@gmail.com>
2017-08-10 12:52:38 -0400

    sytax switch

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
-  $.ajax({
+  jQuery.ajax({
     url: 'https://www.codeschool.com/users/bulwark.json',
     dataType: 'jsonp',
     success: function(response){
       $.each(response.courses.completed, function(index, info){
         var badge = $('<div></div>');
-        $(badge).addClass('.course');
+        badge.addClass('.course');
         var h3Title = $('<h3>'+ info.title +'</h3>');
         var image = $('<img></img>');
-        $(image).attr('src', info.badge);
+        image.attr('src', info.badge);
         var link = $('<a></a>');
-        $(link).attr('href', info.url);
-        $(link).attr('target', '_blank');
-        $(link).addClass('.btn').addClass('.btn-primary');
-        $(link).text('See Courses');
-        $(badge).append(h3Title);
-        $(badge).append(image);
-        $(badge).append(link);
+        link.attr('href', info.url);
+        link.attr('target', '_blank');
+        link.addClass('.btn').addClass('.btn-primary');
+        link.text('See Courses');
+        badge.append(h3Title).append(image).append(link);
         $("#badges").append(badge);
       });
     }
