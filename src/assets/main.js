315e16bbb70ca3419a9d58c42196e1f6840b2299
Brian Sullivan <dietrik@gmail.com>
2017-08-10 12:29:42 -0400

    Add badges
    Should add badges with title, image, and link to the correct course

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/bulwark.json',
+    datatype: 'jsonp',
+    success: function(response){
+      $.each(response.courses.completed, function(title, url, badge){
+        var badge = $('<div></div>');
+        $(badge).addClass('course');
+        var h3Title = $('<h3>'+ title +'</h3>');
+        var image = $('<img></img>');
+        $(image).attr('src', badge);
+        var link = $('<a></a>');
+        $(link).attr('href', url);
+        $(link).attr('target', '_blank');
+        $(link).addClass('btn').addClass('btn-primary');
+        $(link).text('See Courses');
+        $(badge).append(h3Title);
+        $(badge).append(image);
+        $(badge).append(link);
+        $("#badges").append(badge);
+      });
+    }
+  })
 });
