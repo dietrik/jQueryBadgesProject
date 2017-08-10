17b6fb6a15a633348c61495ef0fc56d67d15a29f
Brian Sullivan <dietrik@gmail.com>
2017-08-10 12:50:25 -0400

    fixed
    fixed typo with ajax call and incorrect each function

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,18 +1,18 @@
-$(function() {
+$(document).ready(function() {
   $.ajax({
     url: 'https://www.codeschool.com/users/bulwark.json',
-    datatype: 'jsonp',
+    dataType: 'jsonp',
     success: function(response){
-      $.each(response.courses.completed, function(title, url, badge){
+      $.each(response.courses.completed, function(index, info){
         var badge = $('<div></div>');
-        $(badge).addClass('course');
-        var h3Title = $('<h3>'+ title +'</h3>');
+        $(badge).addClass('.course');
+        var h3Title = $('<h3>'+ info.title +'</h3>');
         var image = $('<img></img>');
-        $(image).attr('src', badge);
+        $(image).attr('src', info.badge);
         var link = $('<a></a>');
-        $(link).attr('href', url);
+        $(link).attr('href', info.url);
         $(link).attr('target', '_blank');
-        $(link).addClass('btn').addClass('btn-primary');
+        $(link).addClass('.btn').addClass('.btn-primary');
         $(link).text('See Courses');
         $(badge).append(h3Title);
         $(badge).append(image);
